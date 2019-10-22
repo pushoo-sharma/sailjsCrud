fb6be49281e3e3f7dc17f0895f512ba8a608f771
pushoo-sharma <sharma1997pushpak@gmail.com>
2019-10-22 21:39:17 +0530

    first cntrl

---
+++ sails-crud-app/api/controllers/ArticlesController.js
@@ -0,0 +1,13 @@
+/**
+ * ArticlesController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  list : (req, res) => {
+      res.send('hello sails.js');
+  }
+};
+
